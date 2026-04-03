d3f9c9722c33c0b11ba448f102f9f1d6a70eae0b
mohammedsalam96dcomp <mohammedfaisalsalam@gmail.com>
2026-04-03 11:31:11 -0400

    did i really care this much about the mom script?

--- include/constants/vars.h
+++ include/constants/vars.h
@@ -102,8 +102,8 @@
 
 #define VAR_TWINLEAF_BARRY_STATE                         0x408B // Unused Var
 #define VAR_TWINLEAF_INTRO_STATE                         0x4091 // Unused Var
+#define VAR_TWINLEAF_MOM_STATE                           0x4050
 
-#define VAR_LITTLEROOT_TOWN_STATE                        0x4050
 #define VAR_OLDALE_TOWN_STATE                            0x4051
 #define VAR_DEWFORD_TOWN_STATE                           0x4052 // Unused Var
 #define VAR_LAVARIDGE_TOWN_STATE                         0x4053
