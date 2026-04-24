c50390fbf1ab821cb92845aef810358f8dba5a3c
mohammedsalam96dcomp <mohammedfaisalsalam@gmail.com>
2026-04-23 20:32:25 -0400

    route 204 trainers

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -11,8 +11,8 @@
 #define TRAINER_TRISTAN                       4
 #define TRAINER_NATALIE                       5
 #define TRAINER_LOGAN                         6
-#define TRAINER_GRUNT_SEAFLOOR_CAVERN_2       7
-#define TRAINER_GRUNT_SEAFLOOR_CAVERN_3       8
+#define TRAINER_SARAH                         7
+#define TRAINER_TYLER                         8
 #define TRAINER_GABRIELLE_1                   9
 #define TRAINER_GRUNT_PETALBURG_WOODS        10
 #define TRAINER_MARCEL                       11
@@ -699,7 +699,7 @@
 #define TRAINER_LISA_AND_RAY                692
 #define TRAINER_CHRIS                       693
 #define TRAINER_DAWSON                      694
-#define TRAINER_SARAH                       695
+#define TRAINER_SARAHC                       695
 #define TRAINER_DARIAN                      696
 #define TRAINER_HAILEY                      697
 #define TRAINER_CHANDLER                    698
